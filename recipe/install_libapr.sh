fdc41e03b4aa78aee1111e5605acd69a09bac8b2
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-03-19 12:18:50 +0100

    cleanup

--- recipe/install_libapr.sh
+++ recipe/install_libapr.sh
@@ -5,30 +5,11 @@ set -x
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && $target_platform == "osx-arm64" ]]; then
-  export ac_cv_file__dev_zero=yes
-  export ac_cv_func_setpgrp_void=yes
-  export apr_cv_process_shared_works=yes
-  export apr_cv_mutex_robust_shared=no
-  export apr_cv_mutex_recursive=yes
-  export apr_cv_tcp_nodelay_with_cork=no
-  export ac_cv_sizeof_struct_iovec=16
-  export ap_cv_atomic_builtins=yes
-  export apr_cv_gai_addrconfig=yes
-  export ac_cv_sizeof_pid_t=4
-fi
-
-autoreconf -vfi
-
 ./configure --prefix="${PREFIX}" --host="${HOST}"
 
 make
 make install
 
-# The installed libtool ($PREFIX/build-1/libtool) contains hardcoded absolute paths
-# to the compiler in $BUILD_PREFIX (e.g. .../build_env/bin/x86_64-conda-linux-gnu-cc).
-# That absolute path won't exist for downstream packages or end users. Strip the
-# directory so libtool uses just the compiler name, which will be found on $PATH.
 # Strip hardcoded BUILD_PREFIX paths from libtool so downstream packages
 # find the compiler on $PATH instead of an absolute build-env path.
 sed "s|${BUILD_PREFIX}/bin/||g" "${PREFIX}/build-1/libtool" > "${PREFIX}/build-1/libtool.tmp"
--- recipe/install_libaprutil.bat
+++ recipe/install_libaprutil.bat
@@ -2,7 +2,8 @@ cmake -GNinja ^
     -S%SRC_DIR% ^
     -B%SRC_DIR%\build ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 if errorlevel 1 exit 1
 
 cmake --build %SRC_DIR%\build -- install
