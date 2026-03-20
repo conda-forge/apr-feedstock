6cb0305ae91df4af01ea73f963ad6d7b0fff728a
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-03-20 07:44:46 +0100

    ..

--- recipe/install_libapr.sh
+++ recipe/install_libapr.sh
@@ -5,12 +5,13 @@ set -x
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build
 
+# Use just the compiler basename so libtool doesn't embed absolute build-env
+# paths that won't exist for downstream outputs or end users.
+export CC=$(basename "$CC")
+export CPP=$(basename "$CPP")
+export CXX=$(basename "${CXX:-c++}")
+
 ./configure --prefix="${PREFIX}" --host="${HOST}"
 
 make
 make install
-
-# Strip hardcoded absolute paths to conda build-env compilers in the installed
-# libtool, so downstream outputs/packages find them on $PATH instead.
-sed "s|[^ ]*/build_env/bin/||g" "${PREFIX}/build-1/libtool" > "${PREFIX}/build-1/libtool.tmp"
-mv "${PREFIX}/build-1/libtool.tmp" "${PREFIX}/build-1/libtool"
